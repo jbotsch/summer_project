3a56f4de48b49f1df0f43ecd7e1125e6021a0733
jbotsch <botschjoseph@gmail.com>
2014-06-26 15:04:26 -0400

    first commit
    modifications to display

---
+++ nasdaq_twitter_compare/config.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+  consumer_key: 'wi6olM4GrATSQliSmRHCTfzwU',
+  consumer_secret: 'kWSlCYa9rvaA5hXHof501VWUdE96wL8CyQwkAnTDMqKPRPPRhU',
+  access_token: '332315570-nVpBdLpxfm44GdwqJlk3SSQO1bx98Lr993QLWdTQ',
+  access_token_secret: 'w552xDnuIAJVMein3qhPhDmaTGlEPmdS9Qt17sPolElig'
+}
+
+module.exports = twitterKeys
